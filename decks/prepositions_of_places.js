4fc806d864f9eeb64d9ef229e2f2a9d5eea15c45
Anton Gladyshev <returnstrike@yandex.ru>
2022-12-02 23:14:54 +0200

    added more examples for place preps

--- decks/prepositions_of_places.js
+++ decks/prepositions_of_places.js
@@ -1,8 +1,8 @@
 cards = [
- {"question":"We gonna go ... %arg1",
-  "answer": "to",
+ {"question":"She is ... %arg1",
+  "answer": "at",
   "radios": ["on", "at", "in"],
-  "arg1": ["school", "university", "prison", "hospital", "church", "bed", "work", "home"]},
+  "arg1": ["school", "university", "prison", "hospital", "college", "church", "bed", "work", "home"]},
  {"question":"We should wait ... %arg1",
   "answer": "at",
   "radios": ["on", "at", "in"],
@@ -17,6 +17,91 @@ cards = [
   "answer": "at",
   "radios": ["on", "at", "in"],
   "arg1": ["the front desk", "the rear desk"]
+ },
+ {"question":"She was reading ... %arg1",
+  "answer": "in",
+  "radios": ["on", "at", "in"],
+  "arg1": ["the garden", "the house"]
+ },
+ {"question":"The statue of Petr the Great located ... %arg1",
+  "answer": "in",
+  "radios": ["on", "at", "in"],
+  "arg1": ["Moscow", "Saint-Petersburg", "Nizhnii Novgorod"]
+ },
+ {"question":"French language is official ... %arg1",
+  "answer": "in",
+  "radios": ["on", "at", "in"],
+  "arg1": ["France", "Tunisia", "Mali"]
+ },
+ {"question":"The key was ... %arg1",
+  "answer": "in",
+  "radios": ["on", "at", "in"],
+  "arg1": ["a box", "my pocket"]
+ },
+ {"question":"The money was ... %arg1",
+  "answer": "in",
+  "radios": ["on", "at", "in"],
+  "arg1": ["my wallet", "my pocket"]
+ },
+ {"question":"I lost my keys ... %arg1",
+  "answer": "in",
+  "radios": ["on", "at", "in"],
+  "arg1": ["a building", "my car"]
+ },
+ {"question":"I lost my keys ... %arg1",
+  "answer": "in",
+  "radios": ["on", "at", "in"],
+  "arg1": ["a building", "my car"]
+ },
+ {"question":"There is a painting ... %arg1",
+  "answer": "on",
+  "radios": ["on", "at", "in"],
+  "arg1": ["the wall", "the door", "the ceiling"]
+ },
+ {"question":"There is a picture ... %arg1",
+  "answer": "on",
+  "radios": ["on", "at", "in"],
+  "arg1": ["the cover", "the carpet", "a page"]
+ },
+ {"question":"There is a puddle ... %arg1",
+  "answer": "on",
+  "radios": ["on", "at", "in"],
+  "arg1": ["the floor"]
+ },
+ {"question":"He is ... %arg1",
+  "answer": "at",
+  "radios": ["on", "at", "in"],
+  "arg1": ["the top", "the bottom", "at reception"]
+ },
+ {"question":"We both were ... %arg1 this day",
+  "answer": "in",
+  "radios": ["on", "at", "in"],
+  "arg1": ["a car", "a taxi", "a helicopter", "a boat"]
+ },
+ {"question":"We talked ... %arg1",
+  "answer": "in",
+  "radios": ["on", "at", "in"],
+  "arg1": ["a lift", "an elevator"]
+ },
+ {"question":"There is the Moon ... %arg1",
+  "answer": "in",
+  "radios": ["on", "at", "in"],
+  "arg1": ["the sky"]
+ },
+ {"question":"The interview was published ... %arg1",
+  "answer": "in",
+  "radios": ["on", "at", "in"],
+  "arg1": ["the newspaper"]
+ },
+ {"question":"They had killed a few people ... %arg1",
+  "answer": "in",
+  "radios": ["on", "at", "in"],
+  "arg1": ["a row"]
+ },
+ {"question":"We met each other ... %arg1",
+  "answer": "in",
+  "radios": ["on", "at", "in"],
+  "arg1": ["Oxford Street"]
  },
 ]
 
