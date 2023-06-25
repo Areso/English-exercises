2fb52db62048853653a0814ef01a62cfe3f39be9
Anton Gladyshev <returnstrike@yandex.com>
2023-06-25 23:29:32 +0300

    added two more cards

--- decks/synonyms.js
+++ decks/synonyms.js
@@ -1,13 +1,21 @@
 topic = "Substitute underlying word with a synonym.";
 cards = [
  {"question":"I see a _big_(330th) house across the street",
+  "answer": ["huge", "giant", "enormous", "monstrous", "large", "gigantic", "giant-size"],
+  "question_word": "big"
+ },
+ {"question":"I see a _tall_(330th) house across the street",
   "answer": ["huge", "giant", "enormous"],
   "question_word": "big"
  },
- {"question":"I see a _small_(350th) mouse under the table",
-  "answer": ["little", "tiny"],
-  "question_word": "small"
- }
+ {"question":"I see a _high_(164th) office building",
+  "answer": ["big", "tall", "towering", "colossal", "giant", "gigantic", "monstrous"],
+  "question_word": "high"
+ },
+ {"question":"I see a _high_(164th) office building",
+  "answer": ["big", "tall", "towering", "colossal", "giant", "gigantic", "monstrous"],
+  "question_word": "high"
+ },
 ]
 
 include("js/engine.js",function(){
