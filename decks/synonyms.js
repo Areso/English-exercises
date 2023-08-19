03a85c608a23343cd6bffca85e5e511aa7ee62d8
Anton Gladyshev <returnstrike@yandex.com>
2023-08-20 00:49:38 +0300

    negative prefixes topic added

--- decks/synonyms.js
+++ decks/synonyms.js
@@ -4,17 +4,65 @@ cards = [
   "answer": ["huge", "giant", "enormous", "monstrous", "large", "gigantic", "giant-size"],
   "question_word": "big"
  },
- {"question":"I see a _tall_(330th) house across the street",
-  "answer": ["huge", "giant", "enormous"],
-  "question_word": "big"
- },
  {"question":"I see a _high_(164th) office building",
   "answer": ["big", "tall", "towering", "colossal", "giant", "gigantic", "monstrous"],
   "question_word": "high"
  },
- {"question":"I see a _high_(164th) office building",
-  "answer": ["big", "tall", "towering", "colossal", "giant", "gigantic", "monstrous"],
-  "question_word": "high"
+ {"question":"I see a _small_(th) house",
+  "answer": ["little", "tiny"],
+  "question_word": "small"
+ },
+ {"question":"Did you saw how _slow_(th) this car is?",
+  "answer": ["", ""],
+  "question_word": "slow"
+ },
+ {"question":"A girl rides a _horse_",
+  "answer": ["pony", "destoryer", "кляча"],
+  "question_word": "horse"
+ },
+ {"question":"She was very _happy_ that day",
+  "answer": ["in a good mood", "chairful", "glad", "satisfied"],
+  "question_word": "happy"
+ },
+ {"question":"She was very _unhappy_ that day",
+  "answer": ["in a bad", "gloomy", "unsatisfied"],
+  "question_word": "happy"
+ },
+ {"question":"This job was _good_",
+  "answer": ["well-done", "high quality", "well"],
+  "question_word": "good"
+ },
+ {"question":"This job was _bad_",
+  "answer": ["nasty", "poor quality"],
+  "question_word": "bad"
+ },
+ {"question":"I am a _good_ person",
+  "answer": ["kind", "honest"],
+  "question_word": "good"
+ },
+ {"question":"I am a _bad_ person",
+  "answer": ["mean", "nasty", "naughty"],
+  "question_word": "bad"
+ },
+ {"question":"I heard a _talk_ the other day between crew",
+  "answer": ["gossip"],
+  "question_word": "talk"
+ },
+ {"question":"I see these _beatiful_ flowers",
+  "answer": ["good-looking"],
+  "question_word": "beatiful"
+ },
+ {"question":"I see this _ugly_ house",
+  "answer": ["bad-looking", "creepy"],
+  "question_word": "ugly"
+ },
+ {"question":"You are _fast_.",
+  "answer": ["quick"],
+  "question_word": "fast"
+ },
+ {"question":"This picture is very _colorful_",
+  "answer": ["vivid"],
+  "question_word": "colorful"
  },
 ]
 
---
+++ js/eventer.js
@@ -0,0 +1,63 @@
+function polyfill_uuidv4() {
+    return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
+      (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
+    );
+}
+
+function genUUID() {
+  try {
+    uuid = crypto.randomUUID();
+  } catch (error) {
+    uuid = polyfill_uuidv4();
+  }
+  return uuid;
+}
+
+uuid = localStorage.getItem("english_uuid");
+if (uuid === null) {
+  uuid = genUUID();
+  localStorage.setItem("enlish_uuid", uuid);
+}
+
+function sendEvent(eventname, params) {
+    webserver="http://localhost:8091"
+    console.log("sendEvent");
+    console.log(uuid);
+    if (uuid === null || uuid === undefined) {
+        console.log("uuid is null or undefined")
+        console.log("try to load uuid from localStorage")
+        uuid = localStorage.getItem("english_uuid");
+        console.log("uuid from localStorage is ")
+        console.log(uuid)
+    }
+    mylocation = window.location.href;
+
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+      if (this.readyState === 4 && this.status === 200) {
+        console.log(this.responseText)
+      }
+    };
+    dataToParse  = {
+      "project": "english_exercises",
+      "uuid": uuid,
+      "event": eventname,
+      "location": mylocation,
+      "params":params,
+    }
+    //dataToParse=""
+    endpoint     = webserver + "/prc";
+    xhttp.open("POST", endpoint, true);
+    xhttp.setRequestHeader("Content-type", "application/json");
+    xhttp.send(JSON.stringify(dataToParse));
+    console.log("sendEvent");
+}
+
+if (window.location.href.indexOf("index")!==-1){
+  sendEvent("main menu loaded", "");
+}
+if (window.location.href.indexOf("lesson")>0 || window.location.href.indexOf("grammar")>0){
+  //nothing happens
+} else {
+  sendEvent("main menu loaded", "");
+}
