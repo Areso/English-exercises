624bfec58973de107a121cc129db6af5cade8d52
Anton Gladyshev <returnstrike@yandex.com>
2023-08-20 19:59:03 +0300

    new examples

---
+++ decks/negative_prefixes.js
@@ -0,0 +1,139 @@
+//https://www.englishhints.com/prefix-list.html
+topic = "Usage of negative prefixes.";
+grammar = "Use in-, im-, il-, ir- with Latin-derived roots, e.g. 'intolerant'<br>";
+grammar+= "Use il- before l, im before b,m,p, -ir before r<br>";
+grammar+= "Use un- with word of Latin origin with -ed ending, e.g. 'unfounded'<br>";
+grammar+= "Use non. Use as you like, no hyphen needed<br>"
+grammar+= "De- is affixed to denote reversal of an action.<br>"
+grammar+= "Dis- could be used as de-, or as un- affixes<br>"
+grammar+= "A-, ab- used with adjectives ending in -al<br>"
+grammar+= "Anti- means against<br>"
+grammar+= "Mis- used with Old English and some French words, means bad, wrong(ly)<br>"
+cards = [
+ {"question":"It is ... (not possible) to do that",
+  "answer": "impossible"
+ },
+ {"question":"Your homework is ... (not adequate) - you'll have to do it again",
+  "answer": "inadeqaute"
+ },
+ {"question":"It was ... (not responsible) to leave younger brother alone",
+  "answer": "irresposible"
+ },
+ {"question":"It was totally ... (not believable)",
+  "answer": "unbeliavable"
+ },
+ {"question":"It is ... (not immigrant) visa. You can't apply for the resident card",
+  "answer": "nonimmigrant"
+ },
+ {"question":"We need to ... (undo hydrate) it before it starts rotten",
+  "answer": "dehydrate"
+ },
+ {"question":"... (stop regulating) it and you will see this sector blooming",
+  "answer": "deregulate"
+ },
+ {"question":"You need to ...(undo toxicating) in order to feel better",
+  "answer": ["detoxify","detox"]
+ },
+ {"question":"You need to ... (revert assemble) it first",
+  "answer": "disassemble"
+ },
+ {"question":"This is 100% is ... (not normal)",
+  "answer": "abnormal"
+ },
+ {"question":"This ... (working against graviation) engine still works fine. But you need to tune it",
+  "answer": ["antigraviation","antrigrav","antigraviate"]
+ },
+ {"question":"Why you rebooted the server? I was ... (have wrong guide) about the plan",
+  "answer": "misguided"
+ },
+ {"question":"I made wrong decision, because I was ... (informed wrongly)",
+  "answer": "misinformed"
+ },
+ {"question":"You need to ... (undo activating) the countdown timer now!",
+  "answer": "deactivate"
+ },
+ {"question":"You need to ... (code) the message before read",
+  "answer": "decode"
+ },
+ {"question":"You need to ... (undo composing) the task to smaller tasks",
+  "answer": "decompose"
+ },
+ {"question":"You need to ... (undo composing) the task to smaller tasks",
+  "answer": "decompose"
+ },
+ {"question":"You need to ... (undo construcsion) this thing to the ground",
+  "answer": "deconstruct"
+ },
+ {"question":"Antibiotics wiil help you to ... (undo constaminate)",
+  "answer": "decontaminate"
+ },
+ {"question":"You need to ... (undo inflate) this balloon",
+  "answer": "deflate"
+ },
+ {"question":"You need to ... (v. flect) all this attacks",
+  "answer": "deflect"
+ },
+ {"question":"You need to ... (undo form) this ugly mask",
+  "answer": "deform"
+ },
+ {"question":"You need to ... (undo mythologize) this weapon",
+  "answer": "demythologize"
+ },
+ {"question":"This train ... (rails off) yesterday",
+  "answer": ["derail","derailed"]
+ },
+ {"question":"She ... (v. track) from her achievement",
+  "answer": "detract"
+ },
+ {"question":"She ... (not agrees) on that with him",
+  "answer": "disagrees"
+ },
+ {"question":"We agree to ... (not agree) on that",
+  "answer": "disagree"
+ },
+ {"question":"Military plot of ...(affected) group with the current government",
+  "answer": "disaffected"
+ },
+ {"question":"Military plot of ...(affected) group with the current government",
+  "answer": "disaffected"
+ },
+ {"question":"... (void of belief) will collapse this society one day",
+  "answer": "disbelief"
+ },
+ {"question":"... (void of honor) you braught shall be paid off with your blood!",
+  "answer": "dishonor"
+ },
+ {"question":"... (v infect) your wound ASAP",
+  "answer": "disinfect"
+ },
+ {"question":"... (non infection) is in order!",
+  "answer": "disinfection"
+ },
+ {"question":"You should to ... (v inherit) your son because of his dishonor",
+  "answer": "disinherit"
+ },
+ {"question":"... (non loyalty) to your liege is punished by death",
+  "answer": "disloyalty"
+ },
+ {"question":"Being ... (non loyal) to your superior is somewhat common nowadays",
+  "answer": "disloyal"
+ },
+ {"question":"Being ... (non loyal) to your superior is somewhat common nowadays",
+  "answer": "disloyal"
+ },
+ {"question":"... (non please) hime once and he will hate you forever",
+  "answer": "displease"
+ },
+ {"question":"... (non trust) is very common nowadays",
+  "answer": "distrust"
+ },
+ {"question":"It is ... (non illegal) to be here without proper docs.",
+  "answer": "illegal"
+ },
+
+]
+
+
+include("js/engine.js",function(){
+  //
+});
