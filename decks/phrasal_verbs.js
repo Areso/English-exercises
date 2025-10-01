ede0298d65f66f92e01b1e0fe656839d5c47afd5
Anton Gladyshev <returnstrike@yandex.com>
2025-10-01 14:19:32 +0300

    added phrasal verbs practice

---
+++ decks/phrasal_verbs.js
@@ -0,0 +1,22 @@
+topic   = "Phrasal verbs";
+cards = [
+ {"question":"We going ... to a restaurant tonight",
+  "answer": "out"
+ },
+ {"question":"We are moving ... to this new apartment, finally",
+  "answer": ["in"]
+ },
+ {"question":"Look ...! (Be careful//watch for smth)",
+  "answer": ["out"],
+ },
+ //{"question":"He is ... (good), whe he works alone",
+ // "answer": ["best", "the best"],
+ // "comment": "you don't need the article because you comparing the same thing in different circumstances",
+ // "showcomment": "yes",
+ //},
+]
+
+
+include("js/engine.js",function(){
+  //
+});
