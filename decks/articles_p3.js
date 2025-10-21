1f88042fed5f206f622596d7941b705ba7e570be
Anton Gladyshev <returnstrike@yandex.com>
2025-10-21 21:39:49 +0300

    improved grammar descr for articles p3

--- decks/articles_p3.js
+++ decks/articles_p3.js
@@ -1,7 +1,8 @@
 topic = "Articles and countries.";
 grammar = "Usually we don't need articles with countries<br>";
-grammar+= "However, some countries require def. article: the USA(US), the UK, the GB, etc<br>";
-grammar+= "Rule of article: if country has special word, such as Kingdom/Federation/Republic/<br>"
+grammar+= "However, some countries require def. article: the USA(US), the UK, etc<br>";
+grammar+= "Be careful, though: I live in Great Britain<br>";
+grammar+= "Rule of _the_ article: if country has special word, such as Kingdom/Federation/Republic/<br>"
 cards = [
  {"question":"I live in ... Spain",
   "answer": ["", "no article","-"],
@@ -9,6 +10,10 @@ cards = [
  },
  {"question":"I am from ... US",
   "answer": ["the"],
+  "radios": ["a", "an", "the", "no article"]
+ },
+{"question":"I live in ... Great Britain",
+  "answer": ["", "no article","-"],
   "radios": ["a", "an", "the", "no article"]
  },
  {"question":"Who is the Ruler of ... Kingdom of Spain?",
